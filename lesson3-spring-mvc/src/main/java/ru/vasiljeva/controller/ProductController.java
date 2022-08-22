9c9419bcb2329347dfc1072977ff7275255a6b1b
leamer <wasiljeva@gmail.com>
2022-08-22 17:51:03 +0300

    add product views

--- lesson3-spring-mvc/src/main/java/ru/vasiljeva/controller/ProductController.java
+++ lesson3-spring-mvc/src/main/java/ru/vasiljeva/controller/ProductController.java
@@ -1,36 +1,37 @@
 package ru.vasiljeva.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.RequiredArgsConstructor;
-import ru.vasiljeva.data.Product;
 import ru.vasiljeva.data.ProductRepository;
 
 @Controller
 @RequestMapping("/product")
-@RequiredArgsConstructor
 public class ProductController {
-	    private final ProductRepository productRepository;
 
-	    @GetMapping
-	    public String listPage(Model model) {
-	        model.addAttribute("productss", productRepository.getAll());
-	        return "user";
-	    }
+	@Autowired
+	private ProductRepository productRepository;
 
-	    @GetMapping("/{id}")
-	    public String form(@PathVariable("id") long id, Model model) {
-	        model.addAttribute("user", productRepository.findProductById(id));
-	        return "user_form";
-	    }
+	@GetMapping
+	public String listPage(Model model) {
+		model.addAttribute("products", productRepository.getAll());
+		return "product";
+	}
 
-	    /*@PostMapping
-	    public String saveUser(Product product) {
-	        productRepository.update(product);
-	        return "redirect:/user";
-	    }*/
+	@GetMapping("/{id}")
+	public String form(@PathVariable("id") long id, Model model) {
+		model.addAttribute("product", productRepository.findProductById(id));
+		return "product_form";
+	}
+
+	/*
+	 * @PostMapping public String saveUser(Product product) {
+	 * productRepository.update(product); return "redirect:/user"; }
+	 */
 }
