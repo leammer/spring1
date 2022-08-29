b20e97ac21ba9da23f6d8d91410cbfa2a63f0584
leammer <wasiljeva@gmail.com>
2022-08-29 14:35:45 +0300

    add new maven module

---
+++ lesson5-hibernate/src/main/java/ru/vasiljeva/Main.java
@@ -0,0 +1,84 @@
+package ru.vasiljeva;
+
+import java.util.List;
+
+import org.hibernate.cfg.Configuration;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import ru.vasiljeva.model.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = new Configuration().configure("hibernate.cfg.xml")
+				.buildSessionFactory();
+
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+		// INSERT
+		{
+			System.out.println("Insert products to table");
+
+			entityManager.getTransaction().begin();
+
+			entityManager.persist(new Product("Milk", 10.5));
+			entityManager.persist(new Product("Bread", 18.0));
+			entityManager.persist(new Product("Carrot", 32.50));
+			entityManager.persist(new Product("Onion", 400.00));
+			entityManager.persist(new Product("Chicken", 429.53));
+
+			entityManager.getTransaction().commit();
+		}
+
+		// GET BY ID
+		{
+			entityManager.getTransaction().begin();
+			Product product = entityManager.find(Product.class, 1L);
+			System.out.println("Get product " + product);
+		}
+
+		// GET ALL
+		{
+			List<Product> products = entityManager
+					.createQuery("SELECT p FROM Product p WHERE p.id IN (1, 2)", Product.class).getResultList();
+			for (Product product : products) {
+				System.out.println(product);
+			}
+		}
+
+		// UPDATE
+		{
+			Product product = entityManager.find(Product.class, 1L);
+			product.setName("New Title");
+
+			entityManager.getTransaction().commit();
+		}
+
+		{
+			entityManager.getTransaction().begin();
+
+			Product product = new Product("Product2", 20.0);
+			product.setId(2L);
+			entityManager.merge(product);
+
+			entityManager.getTransaction().commit();
+		}
+
+		// DELETE
+		{
+			entityManager.getTransaction().begin();
+			Product product = entityManager.find(Product.class, 2L);
+			entityManager.remove(product);
+			
+			entityManager.createQuery("DELETE FROM Product p where p.id = 3").executeUpdate();
+
+			entityManager.getTransaction().commit();
+
+		}
+		entityManager.close();
+		entityManagerFactory.close();
+
+	}
+
+}
---
+++ lesson5-hibernate/src/main/java/ru/vasiljeva/model/Product.java
@@ -0,0 +1,37 @@
+package ru.vasiljeva.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "Product")
+@NoArgsConstructor
+public class Product {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "title", length = 30)
+	private @NonNull String name;
+
+	@Column(name = "price", length = 10)
+	private Double cost;
+
+	public Product(@NonNull String name, Double cost) {
+		super();
+		this.name = name;
+		this.cost = cost;
+	}
+}
