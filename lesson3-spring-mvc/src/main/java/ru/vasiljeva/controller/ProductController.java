6a709d35800d5c47ed6ce3b425ea3ee8924d9604
leammer <wasiljeva@gmail.com>
2022-08-29 20:54:06 +0300

    update web forms

--- lesson3-spring-mvc/src/main/java/ru/vasiljeva/controller/ProductController.java
+++ lesson3-spring-mvc/src/main/java/ru/vasiljeva/controller/ProductController.java
@@ -2,6 +2,8 @@ package ru.vasiljeva.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -33,13 +35,14 @@ public class ProductController {
 	}
 
 	@GetMapping("/{id}")
-	public String getProductById(@PathVariable("id") long id, Model model) {
+	public String getProductById(@PathVariable("id") long id, Model model, HttpServletResponse response) {
 		log.info("get product by id " + id);
 
 		Product product = repo.getById(id);
 		if (product == null) {
-			model.addAttribute("error", "Product by such id couldn't be found");
-			return "error";
+			model.addAttribute("message", "Product by such id couldn't be found");
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			return "error_form";
 		}
 		model.addAttribute("product", product);
 		return "product_form";
@@ -47,26 +50,18 @@ public class ProductController {
 
 	@GetMapping("/new")
 	public String addNewProduct(Model model) {
-		// model.addAttribute("user", new User(""));
-		return "product_form";
+		return "new_product_form";
 	}
 
 	@PostMapping
-	public String save(@Validated Product product) {
-		log.info("Save product " + product);
-		repo.insert(product);
-		return "redirect:/product";
-	}
-
-	@PutMapping
-	public String update(@Validated Product product) {
-		log.info("Update product " + product);
+	public String saveOrUpdate(@Validated Product product) {
+		log.info("Save or update product " + product);
 		repo.update(product);
 		return "redirect:/product";
 	}
 
-	@DeleteMapping
-	public String remove(Long id) {
+	@DeleteMapping("/{id}")
+	public String remove(@PathVariable("id") Long id) {
 		log.info("Remove product by id " + id);
 		repo.remove(id);
 		return "redirect:/product";
