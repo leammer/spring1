55dba31e4c8a42357899f891293fe9f15d07c1ea
leammer <wasiljeva@gmail.com>
2022-09-03 20:26:06 +0300

    add curl script

---
+++ lesson4-spring-boot/static/curl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo $'Get full product list\n'
+
+curl http://localhost:8090/app/products
+
+echo $'\n\nGet product by id = 5\n'
+curl http://localhost:8090/app/products/5
+
+echo $'\n\nPost new product\n{"title":"new product", "price":"12.33"}\n'
+curl -d '{"title":"new product", "price":"12.33"}' -H "Content-Type: application/json" -X POST http://localhost:8090/app/products
+
+echo $'\n\nDelete product by id = 21\n'
+curl http://localhost:8090/app/products/delete/21
