a97b2176440b7cb3a445bbc47d5698f4d7b17df9
leammer <wasiljeva@gmail.com>
2022-09-04 17:14:10 +0300

    exception handler

--- lesson4-spring-boot/src/main/java/ru/vasiljeva/controller/ProductRestController.java
+++ lesson4-spring-boot/src/main/java/ru/vasiljeva/controller/ProductRestController.java
@@ -1,18 +1,22 @@
 package ru.vasiljeva.controller;
 
-import java.math.BigDecimal;
 import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import ru.vasiljeva.dto.ErrorDto;
 import ru.vasiljeva.dto.ProductDto;
 import ru.vasiljeva.service.ProductService;
 
@@ -41,5 +45,11 @@ public class ProductRestController {
 	@GetMapping("/delete/{id}")
 	public void deleteById(@PathVariable Long id) {
 		service.removeProduct(id);
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDto validationExceptionHandler(MethodArgumentNotValidException ex) {
+		return new ErrorDto("Fix request", ex.getMessage());
 	}
 }
