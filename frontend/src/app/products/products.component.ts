77f278cf6083dc5281711064ae2e11e69e248cf6
leammer <wasiljeva@gmail.com>
2022-09-17 02:34:58 +0300

    products view

--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  status='';
+  products: {id: number, name: string, price: number}[] = 
+		  [{id:1, name:'Milk', price:10}, 
+		   {id:2, name:'Onion', price:20}, 
+		   {id:3, name:'Bread', price:30}];
+ 	
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  addProductHandler() {
+	  console.log('add product...');
+	  this.products.push({id:4, name:'Meat', price:40});
+  }
 }
