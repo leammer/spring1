678740f249722d83ec534a3a02ae6c3dd08c355c
leammer <wasiljeva@gmail.com>
2022-09-17 10:44:33 +0300

    collect product components in common folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
-import { ProductComponent } from './product/product.component';
-import { FiltersComponent } from './filters/filters.component';
+import { ProductComponent } from './products/product/product.component';
+import { FiltersComponent } from './products/filters/filters.component';
 
 @NgModule({
   declarations: [
